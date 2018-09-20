6bf160b356a8a7a4d5e0b1afd9b154a2f291ab3a
Pavitra Shadvani <pavitrashadvani122@gmail.com>
2018-09-20 18:39:58 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,1 @@
-export const firebaseConfig  = {
-	fire: {
-    apiKey: "AIzaSyABh-GnXn4enz3uLequufM_ucfGU66Ppkg",
-    authDomain: "project-1529646837309.firebaseapp.com",
-    databaseURL: "https://project-1529646837309.firebaseio.com",
-    projectId: "project-1529646837309",
-    storageBucket: "project-1529646837309.appspot.com",
-    messagingSenderId: "287349986283"
-	}
-};
+//Add your firebase api key over here
